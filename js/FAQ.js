5ed8ba17534447524079669b0cd4e026a673fe90
erinlehmkuhl <erinlehmkuhl@gmail.com>
2016-03-05 15:34:57 -0800

    additions: more faqs in json

--- js/FAQ.js
+++ js/FAQ.js
@@ -5,23 +5,31 @@ var FAQ = {
 			"question": "How much does a survey cost?",
 			"answer": "The cost for most land surveying work is determined based on the following variables:<br><br>" +
 				"<dfn>Record Search</dfn><br>" +
-				"This varies by (a) the number of parcels involved; and (b) the number of past transactions. (This necessary step is complicated by the casual manner in which land transactions have been handled in the past, resulting in many vague, incomplete, and often contradictory legal descriptions and land records.)<br><br>" +
+				"This varies by (a) the number of parcels involved; and (b) the number of past transactions. " +
+				"(This necessary step is complicated by the casual manner in which land transactions have been handled in the past, " +
+				"resulting in many vague, incomplete, and often contradictory legal descriptions and land records.)<br><br>" +
 
 				"<dfn>Size and Shape of Property</dfn><br>" +
 				"An irregularly shaped parcel has more corners to monument and a longer boundary than a rectangular parcel containing the same area.<br><br>" +
 
 				"<dfn>Sectionalized Survey Work</dfn><br>" +
-				"This could require the survey of the entire section (640 acres +) in which the land being surveyed lies, regardless of the area of the parcel. In some cases a survey of more than one section is required, depending on the location of the parcel in question in relation to the sections shown on the government plat map.<br><br>" +
+				"This could require the survey of the entire section (640 acres +) in which the land being surveyed lies, regardless of the area of the parcel. " +
+				"In some cases a survey of more than one section is required, depending on the location of the parcel in question in relation to the sections " +
+				"shown on the government plat map.<br><br>" +
 
 				"<dfn>Terrain & Vegetation</dfn><br>" +
-				"A level parcel of land is easier to survey than a mountainous parcel. Interference with lines of sight and accessibility complicate field work.<br><br>" +
+				"A level parcel of land is easier to survey than a mountainous parcel. Interference with lines of sight and " +
+				"accessibility complicate field work.<br><br>" +
 
 				"<dfn>Amount of Existing Evidence on the Property</dfn><br>" +
-				"Existing evidence such as iron, wood, or stone monuments, old fences, and occupation lines, witness trees, etc., aid the Surveyor. Their absence may compound difficulties involved in retracing boundaries."
+				"Existing evidence such as iron, wood, or stone monuments, old fences, and occupation lines, witness trees, etc., " +
+				"aid the Surveyor. Their absence may compound difficulties involved in retracing boundaries."
 		},
 		{
 			"question": "What is an easement?",
-			"answer": "An easement is an area of land owned by the property owner, but in which other parties, such as utility companies, may have limited rights granted for a specific purpose. If a utility company owns the rights, they may have a utility line running below the ground, across your property."
+			"answer": "An easement is an area of land owned by the property owner, but in which other parties, such as utility " +
+			"companies, may have limited rights granted for a specific purpose. If a utility company owns the rights, they may " +
+			"have a utility line running below the ground, across your property."
 		},
 
 		{
@@ -30,19 +38,95 @@ var FAQ = {
 		},
 		{
 			"question": "What is a plat?",
-			"answer": "A legal document intended to take a large parcel of land and divide it into smaller parcels of land. A subdivision plat may also create public rights-of-way or easements, and is usually filed with the county clerk & recorder's office."
+			"answer": "A legal document intended to take a large parcel of land and divide it into smaller parcels of land. " +
+			"A subdivision plat may also create public rights-of-way or easements, and is usually filed with the county clerk " +
+			"& recorder's office."
 		},
 		{
 			"question": "How do I find my existing boundaries?",
 			"answer": "You should have a description of your property included in your deed when you bought the property."
 		},
 		{
-			"question": "I just want to put in a fence, what do I need?",
-			"answer": "This needs a proper answer. Erin doesn't know anything about surveys."
+			"question": "What methods do surveyors use?",
+			"answer": "Most Surveyors use electronic distance and angle measuring equipment, as well as the traditional " +
+			"transit and tape. Modern computer systems aid in efficiently gathering measurements and in evaluating all " +
+			"collected evidence required to perform the survey. Global Positioning Systems (G.P.S.) or 'satellite surveying' " +
+			"can provide greater accuracy and efficiency for some surveys. Land Surveyors take pride in using instruments " +
+			"and computers to perform land surveys efficiently, accurately, and cost effectively."
 		},
 		{
 			"question": "How do I chose a land surveyor?",
-			"answer": "Only a Professional Land Surveyor (or Civil Engineer registered prior to 1982) licensed by the State Board for Professional Engineers and Land Surveyors is legally authorized to practice land surveying in the State of California. Most active Land Surveyors are listed in the yellow pages of the telephone directory, or a listing may be obtained from the California Land Surveyors Association. A Land Surveyor is an integral part of a professional team composed of attorneys, engineers, architects, planners, and landscape architects. <br><br> Some land surveying firms offer comprehensive services including some, or all, of the above. Professional expertise can have a significant impact upon the planned use of your property. Select a reputable Land Surveyor in whose skill and judgment you can put your trust. Your selection should be made when you are sure that the professional you have chosen has all of the facts, and is completely aware of your requirements and the requirements of the governmental agency having jurisdiction over the property."
+			"answer": "Only a Professional Land Surveyor (or Civil Engineer registered prior to 1982) licensed by the State " +
+			"Board for Professional Engineers and Land Surveyors is legally authorized to practice land surveying in the State " +
+			"of California. Most active Land Surveyors are listed in the yellow pages of the telephone directory, or a listing " +
+			"may be obtained from the California Land Surveyors Association. A Land Surveyor is an integral part of a " +
+			"professional team composed of attorneys, engineers, architects, planners, and landscape architects. <br><br> Some " +
+			"land surveying firms offer comprehensive services including some, or all, of the above. Professional expertise can " +
+			"have a significant impact upon the planned use of your property. Select a reputable Land Surveyor in whose skill and " +
+			"judgment you can put your trust. Your selection should be made when you are sure that the professional you have " +
+			"chosen has all of the facts, and is completely aware of your requirements and the requirements of the governmental " +
+			"agency having jurisdiction over the property."
+		},
+		{
+			"question": "What kinds of surveys are there?",
+			"answer": "There are many types of surveys including but not limited to:<br><br>" +
+			"<dfn>ALTA/ACSM Survey or Extended Title Insurance Coverage Survey</dfn><br>" +
+			"A survey made for the purpose of supplying a title company and lender with survey and location data" + 
+			"necessary for issuing American Land Title Association or Extended Coverage Title Insurance.<br><br>" +
+
+			"<dfn>Boundary Survey</dfn><br>" +
+			"A survey for the purpose of locating the corners, boundary lines and/or easements of a given parcel of " +
+			"land. This involves record and field research, measurements, and computations to establish boundary lines " +
+			"in conformance with the Professional Land Surveyors Act.<br><br>" +
+
+			"<dfn>Site Planning Survey</dfn><br>" +
+			"A combination of boundary and topographic surveys for preparation of a site plan to be used for designing " +
+			"improvements or developments.<br><br>" +
+
+			"<dfn>Topographic Survey</dfn><br>" +
+			"A survey locating topographic features - natural and man made - such as buildings, improvements, fences, " +
+			"elevations, trees, streams, contours of the land, etc. This type of survey may be required by a governmental " +
+			"agency, or may be used by engineers and/or architects for design of improvements or developments on a site.<br><br>" +
+
+			"<dfn>Subdivision Survey</dfn><br>" +
+			"The subdivision of a tract of land into smaller parcels, showing monumentation and survey data on a map, " +
+			"in conformance with local ordinances and the Subdivision Map Act.<br><br>" +
+
+			"<dfn>Control Survey</dfn><br>" +
+			"Precise location of horizontal and vertical positions of points for use in boundary determination, mapping " +
+			"from aerial photographs, construction staking, and other related purposes.<br><br>" +
+
+			"<dfn>Construction Survey</dfn><br>" +
+			"Construction staking to establish the correct location of structures shown on improvement plans for constructing " +
+			"roads, pipelines, building, etc."
+		},
+		{
+			"question": "Will a Land Surveyor tell me what I own?",
+			"answer": "No. It is your responsibility to furnish the Surveyor with a legal description, current title report, " +
+			"or policy concerning the parcel that you want surveyed. The Surveyor will then locate the property on the ground, " +
+			"and provide you with a record of survey map showing the results of the survey."
+		},
+		{
+			"question": "Will I be shown if there are any encroachments on the property?",
+			"answer": "Yes, if you instruct the Land Surveyor to show encroachments in the area of concern to you."
+		},
+		{
+			"question": "Will I be shown if there are any easements on my property?",
+			"answer": "Yes, if you instruct the Surveyor to do so, and provide a current title report or title policy to use for this " +
+			"purpose. The Surveyor will supply a map, plat, or exhibit showing this information."
+		},
+		{
+			"question": "How will I be shown what has been surveyed?",
+			"answer": "Corners of the property will be marked with stakes, pipes, or other such monuments with the Professional Land " +
+			"Surveyor's license number indicated thereon. The corners on the parcel will be pointed out to you, if requested. " +
+			"A record of survey or corner record will be filed when these monuments are set, indicating dimensions of property " +
+			"lines, monuments, and other relative data as required by the Land Surveyors Act, the client, or others."
+		},
+		{
+			"question": "Why are there conflicting boundary and easement lines?",
+			"answer": "It is often true that boundary disputes and overlaps are a result of legal descriptions which were originally " +
+			"written and recorded without the benefit of the services of a Professional Land Surveyor. It is important to have " +
+			"these lines properly described and surveyed, if necessary, when property or easement lines are created or changed."
 		}
 		]
 };
