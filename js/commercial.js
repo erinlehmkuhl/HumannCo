6aec18c6029061985b86ce78374fb5bbfcc0af77
erinlehmkuhl <erinlehmkuhl@gmail.com>
2016-04-02 22:23:03 -0700

    chore: adding images to commercial page. some other stuff which I don't remember on the other files. probably mild changes/cleanup.

--- js/commercial.js
+++ js/commercial.js
@@ -5,98 +5,205 @@ var infowindow = null;
 
 var mapMarkers = {
 	"publicWorks": [
+		{"name": "City of Lafayette", 
+		"center": {"lat": 37.895141, "lng":  -122.116421}, 
+		"img": "img/thumbnails/publicWorks/lafayette.jpg"},
+
+		{"name": "City of Concord", 
+		"center": {"lat": 37.972443, "lng": -122.009801}, 
+		"img": "img/thumbnails/publicWorks/concord.jpg"},
+
+		{"name": "City of Orinda", 
+		"center": {"lat": 37.884253, "lng": -122.176057}, 
+		"img": "img/thumbnails/publicWorks/orinda.jpg"},
+
+		{"name": "City of Emeryville", 
+		"center": {"lat": 37.839561, "lng": -122.289188}, 
+		"img": "img/thumbnails/publicWorks/emeryville.jpg"},
+
 		{"name": "Kensington Fire Station", 
 		"center": {"lat": 37.908644, "lng": -122.278051}, 
-		"img": "img/thumbnails/kensington.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/publicWorks/kensington.jpg"},
 		
 		{"name": "Lafayette Chamber of Commerce", 
 		"center": {"lat": 37.890238, "lng": -122.121103}, 
-		"img": "img/thumbnails/lafayette.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/publicWorks/lafayetteChamberOfCommerce.jpg"},
 		
 		{"name": "Chabot Space and Science Center", 
 		"center": {"lat": 37.818940, "lng": -122.180661}, 
-		"img": "img/thumbnails/chabot.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/publicWorks/chabot.jpg"},
 		
 		{"name": "Concord Naval Weapon Station", 
 		"center": {"lat": 38.010403, "lng": -121.982449}, 
-		"img": "img/thumbnails/navalWeapons.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/publicWorks/navalWeapons.jpg"},
 		
 		{"name": "Alcatraz Island", 
 		"center": {"lat": 37.827257, "lng": -122.422945}, 
-		"img": "img/thumbnails/alcatraz.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/publicWorks/alcatraz.jpg"},
 
 		{"name": "Lafayette Reservoir", 
 		"center": {"lat": 37.881981, "lng": -122.142629}, 
-		"img": "img/thumbnails/navalWeapons.jpg",
-		"blurb": "description of work done at this site"}
+		"img": "img/thumbnails/publicWorks/lafayetteReservoir.jpg"},
+
+		{"name": "Emeryville Police Station", 
+		"center": {"lat": 37.837072, "lng": -122.303913}, 
+		"img": "img/thumbnails/publicWorks/emeryvillePoliceStation.jpg"},
+
+		{"name": "City of Pleasant Hill", 
+		"center": {"lat": 37.953088, "lng": -122.076173}, 
+		"img": "img/thumbnails/publicWorks/pleasantHill.jpg"},
+
+		{"name": "City of San Ramon", 
+		"center": {"lat": 37.779630, "lng": -121.937097}, 
+		"img": "img/thumbnails/publicWorks/sanRamon.jpg"},
+
+		{"name": "City of San Mateo", 
+		"center": {"lat": 37.542923, "lng": -122.3112967}, 
+		"img": "img/thumbnails/publicWorks/sanMateo.jpg"},
+
+		{"name": "City of Mountain View", 
+		"center": {"lat": 37.405435, "lng": -122.0872197}, 
+		"img": "img/thumbnails/publicWorks/mountainView.jpg"},
+
+		{"name": "Concord Pavilion", 
+		"center": {"lat": 37.959197, "lng": -121.938146}, 
+		"img": "img/thumbnails/publicWorks/concordPavilion.jpg"},
+
+		{"name": "Pleasant Hill Police Department", 
+		"center": {"lat": 37.962284, "lng": -122.068980}, 
+		"img": "img/thumbnails/publicWorks/pleasantHillPoliceDepartment.jpg"}
 
 	],
 	"schools": [
 		{"name": "Contra Costa County Office of Education", 
-		"center": {"lat": 37.935035, "lng": -122.069851}, 
-		"img": "img/thumbnails/stanley.jpg",
-		"blurb": "description of work done at this site"},
+		"center": {"lat": 37.935035, "lng": -122.069851},
+		"img": "img/thumbnails/schools/cccEducation.jpg"},
 		
 		{"name": "Santa Clara Unified School District", 
-		"center": {"lat": 37.357310, "lng": -121.995466}, 
-		"img": "img/thumbnails/santaClara.jpg",
-		"blurb": "description of work done at this site"},
+		"center": {"lat": 37.3956057, "lng": -122.0469593},
+		"img": "img/thumbnails/schools/santaClaraUnified.jpg"},
 		
 		{"name": "Stanley Middle School", 
 		"center": {"lat": 37.887376, "lng": -122.113522}, 
-		"img": "img/thumbnails/stanley.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/schools/stanleyMiddleSchool.jpg"},
 
 		{"name": "Happy Valley School", 
 		"center": {"lat": 37.904749, "lng": -122.142367}, 
-		"img": "img/thumbnails/happyValley.jpg",
-		"blurb": "description of work done at this site"},
-		
+		"img": "img/thumbnails/schools/happyValley.jpg"},
+
 		{"name": "Burton Valley School", 
 		"center": {"lat": 37.862528, "lng": -122.093597}, 
-		"img": "img/thumbnails/happyValley.jpg",
-		"blurb": "description of work done at this site"},
-		
+		"img": "img/thumbnails/schools/burton.jpg"},
+
 		{"name": "Dahl Elementary", 
 		"center": {"lat": 37.288519, "lng": -121.839535}, 
-		"img": "img/thumbnails/happyValley.jpg",
-		"blurb": "description of work done at this site"}
+		"img": "img/thumbnails/schools/dahl.jpg"},
+
+		{"name": "Walnut Creek School District", 
+		"center": {"lat": 37.908848, "lng": -122.055817}, 
+		"img": "img/thumbnails/schools/walnutCreekUnified.jpg"},
+
+		{"name": "Palomares School", 
+		"center": {"lat": 37.695180, "lng": -122.025352}, 
+		"img": "img/thumbnails/schools/palomares.jpg"},
+
+		// {"name": "Parkside School", 
+		// "center": {"lat": 37.288519, "lng": -121.839535}, 
+		// "img": "img/thumbnails/schools/happyValley.jpg"},
+
+		{"name": "Green Valley Elementary School", 
+		"center": {"lat": 37.833208, "lng": -121.979520}, 
+		"img": "img/thumbnails/schools/greenValley.jpg"},
+
+		{"name": "UC Berkeley-Animal Care", 
+		"center": {"lat": 37.870036, "lng": -122.268538}, 
+		"img": "img/thumbnails/schools/berkeley.jpg"},
+
+		{"name": "Carlmont High School", 
+		"center": {"lat": 37.505269, "lng": -122.288827}, 
+		"img": "img/thumbnails/schools/carlmont.jpg"},
+
+		{"name": "Greenbrook School", 
+		"center": {"lat": 37.791232, "lng": -121.974216}, 
+		"img": "img/thumbnails/schools/greenbrook.jpg"},
+
+		{"name": "Martin Luther King Middle School", 
+		"center": {"lat": 37.881670, "lng": -122.278132}, 
+		"img": "img/thumbnails/schools/mlk.jpg"},
+
+		{"name": "Las Lomas High School", 
+		"center": {"lat": 37.890369, "lng": -122.056258}, 
+		"img": "img/thumbnails/schools/lasLomas.jpg"},
+
+		// {"name": "Neil Armstrong Middle School", 
+		// "center": {"lat": 37.288519, "lng": -121.839535}, 
+		// "img": "img/thumbnails/schools/happyValley.jpg"},
+
+		{"name": "Hillsdale High School", 
+		"center": {"lat": 37.532872, "lng": -122.309776}, 
+		"img": "img/thumbnails/schools/hillsdale.jpg"},
+
+		// {"name": "Twain Harte Elementary", 
+		// "center": {"lat": 37.288519, "lng": -121.839535}, 
+		// "img": "img/thumbnails/schools/happyValley.jpg"},
+
+		{"name": "Family Early Learning Center", 
+		"center": {"lat": 37.351011, "lng": -121.873100}, 
+		"img": "img/thumbnails/schools/familyEarlyLearning.jpg"},
+
+		{"name": "Campolindo High School", 
+		"center": {"lat": 37.288519, "lng": -121.839535}, 
+		"img": "img/thumbnails/schools/campolindo.jpg"},
+
+		{"name": "La Paloma High School", 
+		"center": {"lat": 37.920269, "lng": -121.681303}, 
+		"img": "img/thumbnails/schools/paloma.jpg"},
+
+		// {"name": "Powers Childcare", 
+		// "center": {"lat": 37.288519, "lng": -121.839535}, 
+		// "img": "img/thumbnails/schools/happyValley.jpg"},
+
+		{"name": "Diablo Valley College Footbridge",
+		"center": {"lat": 37.968297, "lng": -122.071860}, 
+		"img": "img/thumbnails/schools/dvcFootbridge.jpg"},
+
+		{"name": "Foothill High School", 
+		"center": {"lat": 37.671990, "lng": -121.918279}, 
+		"img": "img/thumbnails/schools/foothillHighSchool.jpg"},
+
+		// {"name": "Curtis Jr. High School", 
+		// "center": {"lat": 37.288519, "lng": -121.839535}, 
+		// "img": "img/thumbnails/schools/happyValley.jpg"},
+
+		{"name": "Ceasar Chavez Middle School", 
+		"center": {"lat": 37.641812, "lng": -122.023127}, 
+		"img": "img/thumbnails/schools/cesarChavez.jpg"}
+
 	],
 	"churches": [
 		{"name": "St. Andrew Catholic Church", 
 		"center": {"lat": 37.676661, "lng": -122.473898}, 
-		"img": "img/thumbnails/stAndrews.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/churches/saintAndrews.jpg"},
 		
 		{"name": "Guru Granth Sahib Foundation", 
 		"center": {"lat": 37.679680, "lng": -122.065493}, 
-		"img": "img/thumbnails/guruGranth.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/churches/guruGranth.jpg"},
 		
 		{"name": "St. Mina Coptic Church", 
 		"center": {"lat": 37.970601, "lng": -122.012505}, 
-		"img": "img/thumbnails/LOPC.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/churches/LOPC.jpg"},
 
 		{"name": "St John Vianney Catholic Church", 
 		"center": {"lat": 37.919448, "lng": -122.043034}, 
-		"img": "img/thumbnails/stAndrews.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/churches/saintAndrews.jpg"},
 
 		{"name": "Our Savior Lutheran Church", 
 		"center": {"lat": 37.671003, "lng": -121.752874}, 
-		"img": "img/thumbnails/stAndrews.jpg",
-		"blurb": "description of work done at this site"},
+		"img": "img/thumbnails/churches/saintAndrews.jpg"},
 
 		{"name": "St. Perpetua Catholic Church", 
 		"center": {"lat": 37.881331, "lng": -122.113344}, 
-		"img": "img/thumbnails/stAndrews.jpg",
-		"blurb": "description of work done at this site"}
+		"img": "img/thumbnails/churches/saintAndrews.jpg"}
 	]
 };
 
@@ -231,7 +338,7 @@ var getCategories = function() {
 
 var makeCommercialThumbnail = function(attachmentPoints, numPerHeading, clickInfo) {
 //attachmentPoints: array of 3 jquery #id objects. use '' to skip a section. order: publicWorks, School, Church
-//numPerHeading: integer will do (number of thumbnails per section)
+//numPerHeading: integer (number of thumbnails per section)
 //clickInfo: send "true" if this function was called from a click (instead of initial page load)
 
 	for (var i = 0; i < attachmentPoints.length; i++) {//# of section headings
@@ -259,25 +366,32 @@ var makeCommercialThumbnail = function(attachmentPoints, numPerHeading, clickInf
 			divThumb.appendChild(divCapt);
 			divCapt.appendChild(title);
 			divTop.appendChild(divThumb);
+
+			//construct row 
+			var row = document.createElement("DIV");
+			row.classList.add("row");
 			
 			//specifics per thumbnail
-			if (i == 0) {//publicWorks
+			if (i == "0" && attachmentPoints[i] != "") {//publicWorks
 				img.src = mapMarkers.publicWorks[j].img;
 				img.alt = mapMarkers.publicWorks[j].name;
 				title.innerHTML = mapMarkers.publicWorks[j].name;
 
-			} else if (i == 1) {//schools
+			} else if (i == "1"  && attachmentPoints[i] != "") {//schools
 				img.src = mapMarkers.schools[j].img;
-				img.alt = mapMarkers.publicWorks[j].name;
+				img.alt = mapMarkers.schools[j].name;
 				title.innerHTML = mapMarkers.schools[j].name;
 
-			} else if (i == 2) {//churches
+			} else if (i == "2" && attachmentPoints[i] != "") {//churches
 				img.src = mapMarkers.churches[j].img;
-				img.alt = mapMarkers.publicWorks[j].name;
+				img.alt = mapMarkers.churches[j].name;
 				title.innerHTML = mapMarkers.churches[j].name;
 			}
 
 			//append generic thumbnail
+			if (j % 4 == 0 || j == 0) {
+				$("#"+attachmentPoints[i]).append(row);
+			}
 			$("#"+attachmentPoints[i]).append(divTop);
 		}
 	}
@@ -289,12 +403,14 @@ var toggleThumbnails = function(clicked_id, moreButtonPushed, eventClick) {
 	var attachmentPoints = ["publicWorksShowMore", "schoolsShowMore", "churchesShowMore"];
 	var arrayElem;
 	var clickTrue = true;
-	var numPerHeading = Math.min(mapMarkers.publicWorks.length, mapMarkers.schools.length, mapMarkers.churches.length);
 
 	//cycle through each heading to find which one got clicked
 	for (var i = 0; i < attachmentPoints.length; i++) {
 		//if what is clicked matches a attachPointList entry
 		if (attachmentPoints[i].indexOf(clicked_id) > -1) {
+			//decide how many thumbnails to make per section
+			var numPerHeading = mapMarkers[clicked_id].length;
+			console.log("numPerHeading: ", numPerHeading);
 			//log the name of the item pressed (publicWorks, schools or churches)						
 			arrayElem = attachmentPoints[i];
 			//clear out the array but keep placeholders so there are still three spots
