a63a136a35e35d1ad3949d670b6a9ad82a99d14d
erinlehmkuhl <erinlehmkuhl@gmail.com>
2016-04-10 20:31:35 -0700

    feature: modal working on survey page

--- js/modalSurvey.js
+++ js/modalSurvey.js
@@ -1,7 +1,7 @@
-var modalData = {
+var modalSurveyData = {
   "survey": [
     {"title": "Topographic",
-    "paragraph": "Topographic Surveys are used to identify and map the contours of the ground and existing features on the surface of the earth or slightly above or below the earth's surface (i.e. trees, buildings, streets, walkways, manholes, utility poles, retaining walls, etc."},
+    "paragraph": "Topographic Surveys are used to identify and map the contours of the ground and existing features on the surface of the earth or slightly above or below the surface of the earth (i.e. trees, buildings, streets, walkways, manholes, utility poles, retaining walls, etc."},
 
     {"title": "Building Location",
     "paragraph": "The location survey shows the approximate location of the dwelling and other structures within the property boundaries. A location survey will also determine whether any adjoining property owner is encroaching on the property you are purchasing such as by having a building extending across the neighbor’s property onto your property, or structures on the property you are purchasing that appear to encroach over building restriction lines, property lines, or drainage and utility easements."},
@@ -10,7 +10,7 @@ var modalData = {
     "paragraph": "An ALTA survey is a boundary survey prepared to a set of minimum standards that have been jointly prepared and adopted by the ALTA/ACSM. The survey shows improvements, easements, rights-of-way and other elements impacting land ownership."},
 
     {"title": "FEMA Elevation Certificates",
-    "paragraph": "An Elevation Certificate is an important tool that documents your building’s elevation."},
+    "paragraph": "An Elevation Certificate is an important tool that documents the elevation of your building."},
 
     {"title": "Setback and other building permit certificates",
     "paragraph": "In land use, a setback is the distance which a building or other structure is set back from a street or road, a river or other stream, a shore or flood plain, or any other place which is deemed to need protection."},
@@ -18,11 +18,11 @@ var modalData = {
     {"title": "Forensic and surveys relating disputes",
     "paragraph": "No opinions regarding the status of property lines or property corners are provided in a forensic survey, nor are property corners established or re-established as a result of a forensic survey.  A forensic survey may include boundary information as incidental to the focus of the survey, however, no opinions regarding boundaries are provided. A forensic survey is scientific in its methodology.  The final product of the forensic survey is a document that illustrates the evidence discovered during the survey to support and advance a scientific, engineering, and/or legal theory."},
 
-    {"title": "Boundary staking",
+    {"title": "Boundary Staking",
     "paragraph": "Boundary surveying is performed to establish or reestablish a parcel’s boundary lines. Using the parcel’s deed description and/or other title documents, along with the boundary evidence found during field survey, a land surveyor will determine the boundary lines by applying boundary laws, rules, and principles to guide them to a parcel’s accurate boundaries. When the surveyor has determined the final boundaries of the parcel, the parcel’s boundary markers can be set. We use an iron rod set in the ground with a red cap imprinted with our name and license number. This type of survey is sometimes called a staked survey."},
 
     {"title": "Record of Surveys",
-    "paragraph": "Record of Survey. A Record of Survey is an official map (18 x 26) that is reviewed by the County Surveyors Office and then recorded with the County Recorder's Office. This map represents a survey made on the ground and delineates the deed described lines."},
+    "paragraph": "Record of Survey. A Record of Survey is an official map (18 x 26) that is reviewed by the County Surveyors Office and then recorded with the County Recorder. This map represents a survey made on the ground and delineates the deed described lines."},
 
     {"title": "Corner Records",
     "paragraph": "A Corner Record is filed whenever a Land Surveyor places or replaces a property marker or any other marker as provided for under the Professional Land Surveyors Act. As a general rule, the Corner Record is used to document the placement of a survey monument when the marker already appeared on a previously filed Corner Record or on a recorded ROS or a Subdivision Map."},
@@ -30,12 +30,11 @@ var modalData = {
     {"title": "Lot Line Adjustments",
     "paragraph": "A lot line adjustment is the process that is used to change property lines of existing parcels. The process can be used to do a number of things, such as: combine up to four (4) adjacent parcels into one (1) parcel, alter the boundary between up to four (4) parcels, or reconfigure the shapes of up to four (4) parcels."},
 
-    {"title": "Lot mergers",
+    {"title": "Lot Mergers",
     "paragraph": "something something"},
 
-    {"title": "Easement and deed preparation",
+    {"title": "Easement and Deed Preparation",
     "paragraph": "something something"}  
-  ]
 };
 
 var clickedImage;
@@ -56,6 +55,7 @@ var createModal = function() {
   var modalBody = document.createElement("DIV");
   modalBody.classList.add("modal-body");
   var modalParagraph = document.createElement("P");
+  modalParagraph.classList.add("modal-paragraph");
   var modalFooter = document.createElement("DIV");
   modalFooter.classList.add("modal-footer");
   var modalClose = document.createElement("BUTTON");
@@ -63,16 +63,6 @@ var createModal = function() {
   modalClose.setAttribute("data-dismiss", "modal");
   modalClose.classList.add("close");
   modalClose.innerHTML = "Close";
-
-
-  var survey = getCategories(modalData);
-  for (var i = 0; i < modalData[survey].length; i++) {
-    if (clickedImage == modalData[survey][i].title) {
-      //console.log(modalData[survey][i]);
-      modalTitle.innerHTML = modalData[survey][i].title;
-      modalParagraph.innerHTML = modalData[survey][i].paragraph;
-    }
-  };
 
   $('#surveyModal').append(modalContent);
   modalContent.appendChild(modalHeader);
@@ -85,13 +75,19 @@ var createModal = function() {
   modalFooter.appendChild(modalClose);
 };
 
-//TODO: need to get the h3 value from the thumbnail i clicked so i can
-//compare it to the loop above and when they match, insert the title and paragraph info
+
 var showModal = function() {
+  var clickedImage = event.path[1].id;
   $("#surveyModal").toggle;
-  var $domstuff = event.path[1];
-  console.log($domstuff);
-  console.log($domstuff.children("h3"));
+
+  var survey = getCategories(modalData);
+    for (var i = 0; i < modalData[survey].length; i++) {
+      var jsonData = modalData[survey][i].title.toLowerCase();
+      if ( jsonData.indexOf(clickedImage) > -1 ) {
+        $('.modal-title').text(modalData[survey][i].title);
+        $('.modal-paragraph').text(modalData[survey][i].paragraph);
+      }
+    }
 };
 
 $(document).ready(function(){
