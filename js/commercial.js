9778b5a2e8d0447d39cf8b2893a50d5db291bbab
erinlehmkuhl <erinlehmkuhl@gmail.com>
2016-04-19 19:41:00 -0700

    refactor: click event now in navbar not commercial.js
    and all of the html files are linked appropriately.

--- js/commercial.js
+++ js/commercial.js
@@ -240,47 +240,6 @@ var toggleThumbnails = function(clicked_id, moreButtonPushed, eventClick) {
 	}
 };
 
-//listens for any click and if the event contains: publicWorks, schools or churches 
-//figures out where it came from and toggles collapsed sections appropriately
-addEventListener('click', function (ev) {
-	var clicked_id;
-	var moreButtonPushed = false;
-	var eventClick = ev.target;
-
-	//this click is coming from the navbar.html insert on any page
-    if (eventClick.classList.contains("portfolioButtons")) {
-		//log what button pushed it -- so you know where to attach the thumbnails
-	    clicked_id = eventClick.text;
-	    clicked_id = clicked_id.split(" ").join("");
-	    clicked_id = clicked_id.charAt(0).toLowerCase() + clicked_id.slice(1);
-	    //clicked_id = clicked_id.toString();
-
-	    //re-direct to page and scroll to bookmark
-		window.location.href = "commercial.html#"+clicked_id+"Header";
-
-		//manually open the section (since the button wasn't physically pushed)
-		if (moreButtonPushed == false) {
-    		$("#"+clicked_id+"ShowMore").addClass("in");
-    		moreButtonPushed = true;
-    	}
-
-    //this click is coming from the commercial.html page
-	} else if (eventClick.classList.contains("moreButtons")){
-		clicked_id = eventClick.id;
-		moreButtonPushed = true;
-	}
-
-	//swap up caret for down caret and show/create thumbnails
-	toggleThumbnails(clicked_id, moreButtonPushed, eventClick);
-
-	//manually open the section (since the button wasn't physically pushed)
-	//window.location.href = "#endCommercialPage";
-
-});
-
-
-
-
 var showPDFs = function() {
 	$("#listOfPDFs").toggleClass("hide");
 	$(".spacerGoAway").toggleClass("hide");
